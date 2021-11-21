b61194f144e7a22f435114ae031cd78061942a91
mrinal21 <haldermrinal1996@gmail.com>
2021-11-21 22:19:31 +0530

    hello print

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user',[user::class,'show']);
+
+
+
+
+
+
+
+
+
+
+
+
